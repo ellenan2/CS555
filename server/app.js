ab02262a4f622bbe6e7d2e39278ab9ac5f03b9c5
nwhitt23 <nfwhitt3901@gmail.com>
2023-03-02 18:19:40 -0500

    Removed sockets code

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ const session = require('express-session');
 const cors = require('cors');
 var path = require('path');
 const { decodeToken } = require('./middleware');
-// const server = require('http').createServer(express);
-// const io = require('socket.io')(server);
 
 const port = 3001;
 
@@ -30,9 +28,9 @@ app.use(session({
  * Dummy Validation of session
  */
 
-
 var public = path.join(__dirname, '/public');
 app.use("/public", express.static(public));
+
 /**
  * Middleware Functionality for validating the token
  */
@@ -42,37 +40,6 @@ app.use('/login', decodeToken);
 app.use('/signup', decodeToken);
 app.use('/changeUserPW', decodeToken);
 app.use('/changeUserInfo', decodeToken);
-
-// app.use(async (req, res, next) => {
-//   let date = new Date().toUTCString();
-//   let authStr;
-//   if(!req.session.email){
-//       authStr = "(Non-Authenticated User)";
-//   }else{
-//       authStr = "(Authenticated User)";
-//   }
-//   console.log(`[${date}]: ${req.method} ${req.originalUrl} ${authStr}`);
-//   next();
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('new client connected', socket.id);
-
-//   socket.on('user_join', ({name, room}) => {
-//     console.log('A user joined ' + room + ' their name is ' + name);
-//     socket.join(room);
-//     socket.to(room).emit('user_join', {name, room});
-//   });
-
-//   socket.on('message', ({name, message, room}) => {
-//     console.log(name, message, socket.id);
-//     io.to(room).emit('message', {name, message});
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('Disconnect Fired');
-//   });
-// });
 
 configRoutes(app);
 
