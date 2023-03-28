a7f0712bb4ab01ac5745ded53b8ffe20a505b0da
ellenan2 <114698697+ellenan2@users.noreply.github.com>
2023-03-28 12:50:20 -0400

    Create userProfileTesting.js

---
+++ userProfileTesting.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import UserProfile from "./UserProfile";
+
+describe("UserProfile", () => {
+  it("renders the user profile with correct props", () => {
+    const props = {
+      profilePicture: "https://example.com/profile.jpg",
+      name: "John Doe",
+      email: "john.doe@example.com",
+      phoneNumber: "+1 123-456-7890",
+    };
+    const wrapper = shallow(<UserProfile {...props} />);
+    expect(wrapper.find(".user-profile")).toHaveLength(1);
+    expect(wrapper.find("img").prop("src")).toEqual(props.profilePicture);
+    expect(wrapper.find("h2").at(0).text()).toEqual(props.name);
+    expect(wrapper.find("h2").at(1).text()).toEqual(props.email);
+    expect(wrapper.find("p").text()).toEqual(props.phoneNumber);
+  });
+});
