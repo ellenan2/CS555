096f540b8a0176f29a9a8935990f48914bea6a9c
tmclough <72697891+tmclough@users.noreply.github.com>
2023-03-28 14:50:48 -0400

    testing update

--- client/src/testing/index.js
+++ client/src/testing/index.js
@@ -1,7 +1,10 @@
-
 import { CurrentOffers } from "./currentOffersTesting.js";
 import { offerTesting } from "./offersTesting.js";
 import { UserProfile } from "./userProfileTesting.js";
-
+if (CurrentOffers && offerTesting && UserProfile) {
+  console.log("Testing successful!");
+} else {
+  console.log("Testing failure...");
+}
 
 export { CurrentOffers, offerTesting, UserProfile };
