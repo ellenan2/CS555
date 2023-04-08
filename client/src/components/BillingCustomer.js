9612553f3af49e80cba2a29679bf3e8dccef5c33
Ellen An <ellenan2@gmail.com>
2023-04-07 21:12:12 -0400

    billing for customer side

---
+++ client/src/components/BillingCustomer.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const BillingPage = () => {
+  const [bills, setBills] = useState([]);
+
+  useEffect(() => {
+    const fetchBills = async () => {
+      const response = await fetch("/api/bills"); //ask about db connection (for bills)
+      const data = await response.json();
+      setBills(data);
+    };
+
+    fetchBills();
+  }, []);
+
+  return (
+    <div>
+      <h1>Billing Information</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Service</th>
+            <th>Description</th>
+            <th>Date of Purchase</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bills.map((bill, index) => (
+            <tr key={index}>
+              <td>{bill.service}</td>
+              <td>{bill.description}</td>
+              <td>{bill.date}</td>
+              <td>${bill.amount.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BillingPage;
