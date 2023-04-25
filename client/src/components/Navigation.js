6e0b75699522c6689024e6cc6cd5168853c29e42
nwhitt23 <nfwhitt3901@gmail.com>
2023-04-25 16:19:17 -0400

    Fixes.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -98,57 +98,57 @@ const Auth = () => {
       );
     };
 
-const Auth = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand" to="/">
-        Solaris Webportal
-      </NavLink>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle Navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-              <NavLink className="nav-link" to='/sales'>Dashboard</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to='/profile'>Account</NavLink>
-          </li>
-        </ul>
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item dropdown">
-            <NavLink 
-            className="nav-link dropdown-toggle" 
-            to="#" 
-            id="navbarDropdown" 
-            role="button" 
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false">
-              Account
-            </NavLink>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <button
-                  className="btn btn-sm btn-outline-secondary dropdown-item"
-                  onClick={doSignOut}
-              >
-                Logout
-              </button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+// const Auth = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <NavLink className="navbar-brand" to="/">
+//         Solaris Webportal
+//       </NavLink>
+//       <button
+//         className="navbar-toggler"
+//         type="button"
+//         data-toggle="collapse"
+//         data-target="#navbarSupportedContent"
+//         aria-controls="navbarSupportedContent"
+//         aria-expanded="false"
+//         aria-label="Toggle Navigation"
+//       >
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+//       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//         <ul className="navbar-nav mr-auto">
+//           <li className="nav-item">
+//               <NavLink className="nav-link" to='/sales'>Dashboard</NavLink>
+//           </li>
+//           <li className="nav-item">
+//             <NavLink className="nav-link" to='/profile'>Account</NavLink>
+//           </li>
+//         </ul>
+//         <ul className="navbar-nav ml-auto">
+//           <li className="nav-item dropdown">
+//             <NavLink 
+//             className="nav-link dropdown-toggle" 
+//             to="#" 
+//             id="navbarDropdown" 
+//             role="button" 
+//             data-toggle="dropdown"
+//             aria-haspopup="true"
+//             aria-expanded="false">
+//               Account
+//             </NavLink>
+//             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+//               <button
+//                   className="btn btn-sm btn-outline-secondary dropdown-item"
+//                   onClick={doSignOut}
+//               >
+//                 Logout
+//               </button>
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
 };
 
 export default Navigation;
